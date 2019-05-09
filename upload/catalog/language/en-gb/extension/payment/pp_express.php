e41905f14937fc39e8cecd82e2fa4caee8713944
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-09 17:21:09 -0400

    Create pp_express.php

---
+++ upload/catalog/language/en-gb/extension/payment/pp_express.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['express_text_title']      = 'Confirm order';
+
+// Text
+$_['text_title']              = 'PayPal Express Checkout';
+$_['text_cart']               = 'Shopping Cart';
+$_['text_shipping_updated']   = 'Shipping service updated';
+$_['text_trial']              = '%s every %s %s for %s payments then ';
+$_['text_recurring']          = '%s every %s %s';
+$_['text_recurring_item']     = 'Recurring Item';
+$_['text_length']             = ' for %s payments';
+$_['text_error']			  = 'There was an error while attempting to obtain the ackknowledgment from PayPal Express - %s : %s .';
+
+// Entry
+$_['express_entry_coupon']    = 'Enter your coupon here:';
+
+// Button
+$_['button_express_coupon']   = 'Add';
+$_['button_express_confirm']  = 'Confirm';
+$_['button_express_login']    = 'Continue to PayPal';
+$_['button_express_shipping'] = 'Update shipping';
+
+// Error
+$_['error_heading_title']	  = 'There was an error';
+$_['error_too_many_failures'] = 'Your payment has failed too many times';
+$_['error_unavailable'] 	  = 'Please use the full checkout with this order';
+$_['error_no_shipping']    	  = 'Warning: No Shipping options are available. Please <a href="%s">contact us</a> for assistance!';
+$_['error_request_failed']	  = 'PayPal request failed, please contact the store owner';
+$_['error_pp_express_ack_failure'] = 'There was an error while acknowledging the transaction. Please contact <a href="%s">%s</a> for more information.';
+$_['error_ship_to_state']	  = 'Either the store or PayPal cannot process the order with the selected country name: %s and zone name: %s . Please contact <a href="%s">%s</a> for more information.';
+$_['error_ship_to_state_zone'] = 'Either the store or PayPal cannot process the order with the selected country name: %s as it cannot find the related zone name. Please contact <a href="%s">%s</a> for more information.'; 
+$_['error_no_country']		  = 'The selected country could not be found with this transaction. Please contact <a href="%s">%s</a> for more information.';
+$_['error_order_info']		  = 'The order information could not be found on our database for the order ID: %s . Please contact <a href="%s">%s</a> for more information.';
+$_['error_order_id']		  = 'The current order ID could not be found on our database. Please contact <a href="%s">%s</a> for more information.';
+$_['error_shipping']		  = 'The selected shipping method could not be found. Please contact <a href="%s">%s</a> for more information.';
