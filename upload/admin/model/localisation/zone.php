bb705c2132d9d6f7cd82aba7be7e06b6561c3e8a
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-12 09:35:33 -0400

    Update zone.php

--- upload/admin/model/localisation/zone.php
+++ upload/admin/model/localisation/zone.php
@@ -65,17 +65,9 @@ class ModelLocalisationZone extends Model {
 	}
 
 	public function getZonesByCountryId($country_id) {
-		$zone_data = $this->cache->get('zone.' . (int)$country_id);
-
-		if (!$zone_data) {
-			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone WHERE country_id = '" . (int)$country_id . "' AND status = '1' ORDER BY name");
-
-			$zone_data = $query->rows;
-
-			$this->cache->set('zone.' . (int)$country_id, $zone_data);
-		}
-
-		return $zone_data;
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone WHERE country_id = '" . (int)$country_id . "' AND status = '1' ORDER BY name")->rows;
+		
+		return $query;
 	}
 	
 	public function getZoneWithCountryId($zone_id, $country_id) {
