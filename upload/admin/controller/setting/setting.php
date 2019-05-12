34e19ad79e9a0505db670970f2ad01f0ffb2860f
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-12 06:58:30 -0400

    Update setting.php

--- upload/admin/controller/setting/setting.php
+++ upload/admin/controller/setting/setting.php
@@ -916,9 +916,9 @@ class ControllerSettingSetting extends Controller {
 			$this->error['address'] = $this->language->get('error_address');
 		}
 		
-		if (!isset($this->request->post['zone_id']) || !filter_var($this->request->post['zone_id'], FILTER_VALIDATE_INT)) {
+		if (!isset($this->request->post['zone_id']) || !is_numeric($this->request->post['zone_id'])) {
 			$this->error['zone'] = $this->language->get('error_zone');
-		} elseif (!isset($this->request->post['country_id']) || !filter_var($this->request->post['country_id'], FILTER_VALIDATE_INT)) {
+		} elseif (!isset($this->request->post['country_id']) || !is_numeric($this->request->post['country_id'])) {
 			$this->error['country'] = $this->language->get('error_country');
 		} else {
 			$this->load->model('localisation/zone');
