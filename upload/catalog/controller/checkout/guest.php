b04278010bb82ea241e2ce6c73a312d4025552ec
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-11 21:47:22 -0400

    Update guest.php

--- upload/catalog/controller/checkout/guest.php
+++ upload/catalog/controller/checkout/guest.php
@@ -186,9 +186,9 @@ class ControllerCheckoutGuest extends Controller {
 				$json['error']['city'] = $this->language->get('error_city');
 			}
 
-			if (empty($this->request->post['zone_id']) || !filter_var($this->request->post['zone_id'], FILTER_VALIDATE_INT)) {
+			if (!isset($this->request->post['zone_id']) || !filter_var($this->request->post['zone_id'], FILTER_VALIDATE_INT)) {
 				$this->error['zone'] = $this->language->get('error_zone');
-			} elseif (empty($this->request->post['country_id']) || !filter_var($this->request->post['country_id'], FILTER_VALIDATE_INT)) {
+			} elseif (!isset($this->request->post['country_id']) || !filter_var($this->request->post['country_id'], FILTER_VALIDATE_INT)) {
 				$this->error['country'] = $this->language->get('error_country');
 			} else {
 				$this->load->model('localisation/country');
