644cb41b6ded16787f9fd73d0461bf41c34d685f
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-12 06:57:08 -0400

    Update zone.php

--- upload/admin/controller/localisation/zone.php
+++ upload/admin/controller/localisation/zone.php
@@ -364,9 +364,9 @@ class ControllerLocalisationZone extends Controller {
 			$this->error['name'] = $this->language->get('error_name');
 		}
 		
-		if (!isset($this->request->post['zone_id']) || !filter_var($this->request->post['zone_id'], FILTER_VALIDATE_INT)) {
+		if (!isset($this->request->post['zone_id']) || !is_numeric($this->request->post['zone_id'])) {
 			$this->error['zone'] = $this->language->get('error_zone');
-		} elseif (!isset($this->request->post['country_id']) || !filter_var($this->request->post['country_id'], FILTER_VALIDATE_INT)) {
+		} elseif (!isset($this->request->post['country_id']) || !is_numeric($this->request->post['country_id'])) {
 			$this->error['country'] = $this->language->get('error_country');
 		} else {
 			$this->load->model('localisation/zone');
