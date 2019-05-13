cd4d756f7a470d474a6957cc179ac5ab22053680
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-13 16:22:04 -0400

    Create extension.php

---
+++ upload/catalog/model/setting/extension.php
@@ -0,0 +1,79 @@
+<?php
+class ModelSettingExtension extends Model {
+	function getExtensions($type) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "extension WHERE `type` = '" . $this->db->escape($type) . "'");
+
+		return $query->rows;
+	}
+	
+	// Payment Methods
+	public function getPaymentMethods($payment_address, $total) {		
+		$method_data = array();
+
+		$results = $this->getExtensions('payment');
+
+		$recurring = $this->cart->hasRecurringProducts();
+
+		foreach ($results as $result) {
+			if ($this->config->get('payment_' . $result['code'] . '_status')) {
+				$this->load->model('extension/payment/' . $result['code']);
+
+				$method = $this->{'model_extension_payment_' . $result['code']}->getMethod($payment_address, $total);
+
+				if ($method) {
+					if ($recurring) {
+						if (property_exists($this->{'model_extension_payment_' . $result['code']}, 'recurringPayments') && $this->{'model_extension_payment_' . $result['code']}->recurringPayments()) {
+							$method_data[$result['code']] = $method;
+						}
+					} else {
+							$method_data[$result['code']] = $method;
+					}
+				}
+			}
+		}
+
+		$sort_order = array();
+
+		foreach ($method_data as $key => $value) {
+			$sort_order[$key] = $value['sort_order'];
+		}
+
+		array_multisort($sort_order, SORT_ASC, $method_data);
+		
+		return $method_data;
+	}
+	
+	// Shipping Methods
+	public function getShippingMethods($shipping_address) {		
+		$method_data = array();
+
+		$results = $this->getExtensions('shipping');
+
+		foreach ($results as $result) {
+			if ($this->config->get('shipping_' . $result['code'] . '_status')) {
+				$this->load->model('extension/shipping/' . $result['code']);
+
+				$quote = $this->{'model_extension_shipping_' . $result['code']}->getQuote($shipping_address);
+
+				if ($quote) {
+					$method_data[$result['code']] = array(
+						'title'      => $quote['title'],
+						'quote'      => $quote['quote'],
+						'sort_order' => $quote['sort_order'],
+						'error'      => $quote['error']
+					);
+				}
+			}
+		}
+
+		$sort_order = array();
+
+		foreach ($method_data as $key => $value) {
+			$sort_order[$key] = $value['sort_order'];
+		}
+
+		array_multisort($sort_order, SORT_ASC, $method_data);
+		
+		return $method_data;
+	}
+}
