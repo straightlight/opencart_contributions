db0716e5a212e243f4aa09630efc60be32bb23d3
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-13 18:52:45 -0400

    Create address_validation.php

---
+++ upload/catalog/language/en-gb/mail/address_validation.php
@@ -0,0 +1,11 @@
+<?php
+// Text
+$_['text_subject']      	= '%s';
+$_['text_date_added']   	= 'Date Added:';
+$_['text_validation']		= 'Automated Event - Store Configuration Validation Report';
+$_['text_payment_method'] 	= 'Payment Methods';
+$_['text_shipping_method'] 	= 'Shipping Methods';
+$_['text_method']			= 'Methods';
+$_['text_product']			= 'Products';
+$_['text_report']			= 'Followed are the payment and shipping service providers being affected during checkout:';
+$_['text_footer']			= 'For more information about this issue, please submit an incident request at the <a href="https://forum.opencart.com">Opencart Forum</a> .';
