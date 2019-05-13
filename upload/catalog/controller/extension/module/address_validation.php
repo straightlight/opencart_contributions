9788cbea3304828f9c374c49d027a57fdbce37de
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-13 19:25:27 -0400

    Update address_validation.php

--- upload/catalog/controller/extension/module/address_validation.php
+++ upload/catalog/controller/extension/module/address_validation.php
@@ -44,7 +44,7 @@ class ControllerExtensionModuleAddressValidation extends Controller {
 
 						$method = $this->{'model_extension_payment_' . $result['code']}->getMethod($this->session->data['payment_address'], $total);
 
-						if (!$method && !preg_match('/free/i', $result['code'])) {
+						if (!$method || (float)$total <= 0) {
 							$this->load->language('extension/payment/' . $result['code']);
 							
 							if (!empty($this->language->get('text_title'))) {
@@ -79,7 +79,7 @@ class ControllerExtensionModuleAddressValidation extends Controller {
 
 						$quote = $this->{'model_extension_shipping_' . $result['code']}->getQuote($this->session->data['shipping_address']);
 
-						if (!$quote && !preg_match('/free/i', $result['code'])) {
+						if (!$quote || (float)$total <= 0) {
 							$this->load->language('extension/shipping/' . $result['code']);
 							
 							if (!empty($this->language->get('text_title'))) {
