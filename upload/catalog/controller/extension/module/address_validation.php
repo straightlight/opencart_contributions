770e72c353098b976db8cfc42bf380116e6157a2
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-13 19:33:34 -0400

    Update address_validation.php

--- upload/catalog/controller/extension/module/address_validation.php
+++ upload/catalog/controller/extension/module/address_validation.php
@@ -44,7 +44,7 @@ class ControllerExtensionModuleAddressValidation extends Controller {
 
 						$method = $this->{'model_extension_payment_' . $result['code']}->getMethod($this->session->data['payment_address'], $total);
 
-						if (!$method && (float)$total < 0.1) {
+						if (!$method) {
 							$this->load->language('extension/payment/' . $result['code']);
 							
 							if (!empty($this->language->get('text_title'))) {
