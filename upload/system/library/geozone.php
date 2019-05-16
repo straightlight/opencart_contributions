49147e97b67985a333571d8dcda820f232b6ca68
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-16 07:45:33 -0400

    Create geozone.php

---
+++ upload/system/library/geozone.php
@@ -0,0 +1,55 @@
+<?php
+class Geozone {
+	public function validateGeoZone($registry, $address, $total) {
+		parent::__construct($registry);
+		
+		$this->load->model('localisation/country');
+		
+		if ($this->config->get('payment_alipay_total') > 0 && $this->config->get('payment_alipay_total') > $total) {
+			$status = false;
+		} elseif (!$this->config->get('payment_alipay_geo_zone_id')) {
+			$status = true;
+		} elseif ($this->config->get('payment_alipay_geo_address') == 'geo_zones' && !empty($address['country_id'])) {
+			$country_info = $this->model_localisation_country->getCountry($address['country_id']);
+			
+			if ($country_info && $country_info['status'] && $country_info['postcode_required']) {
+				$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone WHERE country_id = '" . (int)$country_info['country_id'] . "' AND ((zone_id = '" . (int)$address['zone_id'] . "') OR (zone_id = '0')) AND status = '1'")->rows;
+				
+				$country_implode = array();
+				
+				$zone_implode = array();
+				
+				foreach ($query as $result) {
+					$country_implode[] = "country_id = '" . (int)$result['country_id'] . "'";
+					
+					$zone_implode[] = "zone_id = '" . (int)$result['zone_id'] . "'";
+				}
+				
+				if ($country_implode && $zone_implode) {
+					$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_alipay_geo_zone_id') . "' AND (" . implode(" OR ", $country_implode) . ") AND (" . implode(" OR ", $zone_implode) . ")");
+					
+					if ($query->num_rows) {
+						$status = true;
+					} else {
+						$status = false;
+					}
+				} else {
+					$status = false;
+				}
+			}
+			
+		} elseif ($this->config->get('payment_alipay_geo_address') == 'addresses' && !empty($address['postcode']) && !empty($address['country_id'])) {
+			$country_info = $this->model_localisation_country->getCountry($address['country_id']);
+
+			if ($country_info && $country_info['status'] && $country_info['postcode_required']) {
+				$status = true;
+			} else {
+				$status = false;
+			}
+		} else {
+			$status = false;
+		}
+		
+		return $status;
+	}
+}
