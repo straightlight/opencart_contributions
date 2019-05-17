bd889cec1311378c2b929049d163588cdfba56c1
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-17 09:19:51 -0400

    Update geozone.php

--- upload/system/library/geozone.php
+++ upload/system/library/geozone.php
@@ -1,7 +1,9 @@
 <?php
 class Geozone {
+	protected $registry;
+	
 	public function validateGeoZone($registry, $address, $method, $code, $total) {
-		parent::__construct($registry);
+		$this->registry = (object)$registry;
 		
 		$this->load->model('localisation/country');
 		
@@ -13,21 +15,16 @@ class Geozone {
 			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get($method . '_' . $code . '_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . $address['zone_id'] . "' OR zone_id = '0')");
 
 			if ($query->row) {
-				if ($query->row['zone_id'] == 0) {
-					$result = $this->db->query("SELECT * FROM " . DB_PREFIX . "country WHERE country_id = '" . (int)$address['country_id'] . "' AND status = '1'");
+				if (!empty($address['zone_id'])) {
+					$result = $this->db->query("SELECT * FROM " . DB_PREFIX . "country c LEFT JOIN " . DB_PREFIX . "zone z ON (c.country_id = z.country_id) WHERE c.country_id = '" . (int)$address['country_id'] . "' AND z.zone_id = '" . (int)$address['zone_id'] . "' AND c.status = '1' AND z.status = '1'");
 				} else {
-					$result = $this->db->query("SELECT * FROM " . DB_PREFIX . "country c LEFT JOIN " .  DB_PREFIX . "zone z ON (c.country_id = z.country_id) WHERE z.zone_id = '" . (int)$address['zone_id'] . "' AND c.status = '1' AND z.status = '1'");
+					$result = $this->db->query("SELECT * FROM " . DB_PREFIX . "country WHERE country_id = '" . (int)$address['country_id'] . "' AND status = '1'");
 				}
 
-				if ($result->num_rows) {
-					$status = true;
-				} else {
-					$status = false;
-				}
+				$status = ($result->num_rows) ? true : false;
 			} else {
 				$status = false;
 			}
-			
 		} elseif ($this->config->get($method . '_' . $code . '_geo_address') == 'addresses' && !empty($address['postcode']) && !empty($address['country_id'])) {
 			$country_info = $this->model_localisation_country->getCountry($address['country_id']);
 
@@ -41,5 +38,9 @@ class Geozone {
 		}
 		
 		return $status;
+	}
+	
+	public function __get($name) {
+		return $this->registry->get($name);
 	}
 }
