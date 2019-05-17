e28cd8cd0551acf37a6adf7ad9ef66ec04124f94
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-16 21:33:51 -0400

    Update geo_zone_validation.php

--- upload/admin/controller/extension/module/geo_zone_validation.php
+++ upload/admin/controller/extension/module/geo_zone_validation.php
@@ -2,7 +2,7 @@
 class ControllerExtensionModuleGeoZoneValidation extends Controller {
 	// admin/model/localisation/geo_zone/getZone/before
 	public function validate(&$route, &$args) {
-		$query = "SELECT country_id, COUNT(country_id) AS total_countries, zone_id, COUNT(zone_id) AS total_zones FROM " . DB_PREFIX . "zone_to_geo_zone GROUP BY country_id, zone_id HAVING (COUNT(country_id) > 1) AND (COUNT(zone_id) > 1) AND date_modified < DATE_SUB(NOW(), INTERVAL 1 DAY)";
+		$query = "SELECT country_id, COUNT(country_id) AS total_countries, zone_id, COUNT(zone_id) AS total_zones FROM " . DB_PREFIX . "zone_to_geo_zone GROUP BY country_id, zone_id HAVING (COUNT(country_id) > 1) AND (COUNT(zone_id) > 1) AND date_modified < DATE_SUB(NOW(), INTERVAL 1 DAY) LIMIT 50";
 		
 		if ($query->num_rows) {
 			foreach ($query->rows as $result) {
