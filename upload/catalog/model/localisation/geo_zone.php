f5d0166adf75c3d58d53b99878c469a1143ee7bf
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-18 07:51:32 -0400

    Create geo_zone.php

---
+++ upload/catalog/model/localisation/geo_zone.php
@@ -0,0 +1,8 @@
+<?php
+class ModelLocalisationGeoZone extends Model {
+	public function getGetZoneByKey($address, $key) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get($key) . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . $address['zone_id'] . "' OR zone_id = '0')");
+	
+		return $query->row;
+	}
+}
