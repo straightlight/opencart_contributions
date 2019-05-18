c50837b65d379b470bcc7303367ce7942f4aa46d
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-18 17:03:56 -0400

    Update alipay_cross.php

--- upload/admin/language/en-gb/extension/payment/alipay_cross.php
+++ upload/admin/language/en-gb/extension/payment/alipay_cross.php
@@ -9,6 +9,7 @@ $_['text_edit']                      = 'Edit Alipay Pay';
 $_['text_alipay_cross']              = '<a target="_BLANK" href="https://global.alipay.com"><img src="view/image/payment/alipay-cross-border.png" alt="Alipay Pay Website" title="Alipay Pay Website" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_live']                      = 'Live';
 $_['text_sandbox']                   = 'Sandbox';
+$_['text_all_countries']             = 'All Countries';
 
 // Entry
 $_['entry_app_id']                   = 'Partner ID';
