d3e49d60adea34a0150bcd7b15f8065e234a4482
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-18 08:38:40 -0400

    Update geozone.php

--- upload/system/library/geozone.php
+++ upload/system/library/geozone.php
@@ -14,7 +14,7 @@ class Geozone {
 			}
 			
 			$status = false;
-		} elseif ($this->config->get($method . '_' . $code . '_geo_address') == 'geo_zone' && !empty($address['country_id'])) {
+		} elseif ($this->config->get($method . '_' . $code . '_location') == 'geo_zone' && !empty($address['country_id'])) {
 			if (!$this->config->get($method . '_' . $code . '_geo_zone_id')) {
 				if ($this->config->get($method . '_' . $code . '_debug')) {
 					$this->log->write(':: ' . strtoupper($code) . ' DEBUG :: GEO ZONE :: Geo Zone ID not configured!');	
@@ -80,7 +80,7 @@ class Geozone {
 					$status = false;
 				}
 			}
-		} elseif ($this->config->get($method . '_' . $code . '_geo_address') == 'address' && !empty($address['postcode']) && !empty($address['country_id'])) {
+		} elseif ($this->config->get($method . '_' . $code . '_location') == 'address' && !empty($address['postcode']) && !empty($address['country_id'])) {
 			$country_info = $this->model_localisation_country->getCountry($address['country_id']);
 
 			if ($country_info && $country_info['status'] && $country_info['postcode_required']) {
