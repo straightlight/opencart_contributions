aa2ba3a1fb09f38acd41c7684f863d51d7dbd5ee
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-20 12:10:04 -0400

    Create location.php

---
+++ upload/catalog/language/en-gb/api/location.php
@@ -0,0 +1,15 @@
+<?php
+// Text
+$_['text_payment_address_success']			= 'Success: The payment address results was successfully retrieved with the following payment extension modules:';
+$_['text_shipping_address_success']			= 'Success: The shipping address results was successfully retrieved with the following shipping extension modules:';
+$_['text_payment_account_success']			= 'Success: The payment account results was successfully retrieved with the following payment account extension modules:';
+
+// Error
+$_['error_permission'] 						= 'Warning: You do not have permission to access the API!';
+$_['error_customer_group']					= 'Warning: The selected customer group could not be found!';
+$_['error_geo_zone_customer_custom_field']	= 'Warning: The geo zones with the customers custom fields catalog could not be retrieved. Ensure all required payment and shipping extensions are properly configured!';
+$_['error_payment_address']					= 'Warning: The payment address catalog could not be retrieved!';
+$_['error_shipping_address']				= 'Warning: The shipping address catalog could not be retrieved!';
+$_['error_payment_account']					= 'Warning: The payment address catalog could not be retrieved!';
+$_['error_token']							= 'Warning: Session has expired!';
+$_['error_location']						= 'Warning: The Selected location could not be found!';
