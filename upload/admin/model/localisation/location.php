68718571ff298e1d52891449ef3c8c8183fd34d6
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-20 15:30:58 -0400

    Update location.php

--- upload/admin/model/localisation/location.php
+++ upload/admin/model/localisation/location.php
@@ -1,13 +1,13 @@
 <?php
 class ModelLocalisationLocation extends Model {
 	public function addLocation($data) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "location SET name = '" . $this->db->escape($data['name']) . "', address = '" . $this->db->escape($data['address']) . "', geocode = '" . $this->db->escape($data['geocode']) . "', telephone = '" . $this->db->escape($data['telephone']) . "', fax = '" . $this->db->escape($data['fax']) . "', image = '" . $this->db->escape($data['image']) . "', open = '" . $this->db->escape($data['open']) . "', comment = '" . $this->db->escape($data['comment']) . "'");
+		$this->db->query("INSERT INTO " . DB_PREFIX . "location SET name = '" . $this->db->escape($data['name']) . "', address = '" . $this->db->escape($data['address']) . "', geocode = '" . $this->db->escape($data['geocode']) . "', telephone = '" . $this->db->escape($data['telephone']) . "', fax = '" . $this->db->escape($data['fax']) . "', image = '" . $this->db->escape($data['image']) . "', open = '" . $this->db->escape($data['open']) . "', comment = '" . $this->db->escape($data['comment']) . "', store_id = '" . (int)$data['store_id'] . "'");
 	
 		return $this->db->getLastId();
 	}
 
 	public function editLocation($location_id, $data) {
-		$this->db->query("UPDATE " . DB_PREFIX . "location SET name = '" . $this->db->escape($data['name']) . "', address = '" . $this->db->escape($data['address']) . "', geocode = '" . $this->db->escape($data['geocode']) . "', telephone = '" . $this->db->escape($data['telephone']) . "', fax = '" . $this->db->escape($data['fax']) . "', image = '" . $this->db->escape($data['image']) . "', open = '" . $this->db->escape($data['open']) . "', comment = '" . $this->db->escape($data['comment']) . "' WHERE location_id = '" . (int)$location_id . "'");
+		$this->db->query("UPDATE " . DB_PREFIX . "location SET name = '" . $this->db->escape($data['name']) . "', address = '" . $this->db->escape($data['address']) . "', geocode = '" . $this->db->escape($data['geocode']) . "', telephone = '" . $this->db->escape($data['telephone']) . "', fax = '" . $this->db->escape($data['fax']) . "', image = '" . $this->db->escape($data['image']) . "', open = '" . $this->db->escape($data['open']) . "', comment = '" . $this->db->escape($data['comment']) . "' WHERE location_id = '" . (int)$location_id . "', store_id = '" . (int)$data['store_id'] . "'");
 	}
 
 	public function deleteLocation($location_id) {
@@ -54,6 +54,12 @@ class ModelLocalisationLocation extends Model {
 
 		$query = $this->db->query($sql);
 
+		return $query->rows;
+	}
+	
+	public function getLocationByStores($store_id) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "location" WHERE store_id = '" . (int)$store_id . "'");
+		
 		return $query->rows;
 	}
 	
