24398fff25cbf6774c0342aaa42b8d354913b807
straightlight <32371528+straightlight@users.noreply.github.com>
2019-05-21 08:27:44 -0400

    Update geo_zone.php

--- upload/catalog/model/localisation/geo_zone.php
+++ upload/catalog/model/localisation/geo_zone.php
@@ -6,76 +6,64 @@ class ModelLocalisationGeoZone extends Model {
 		return $query->row;
 	}
 	
-	public function getZoneToGeoZoneLocation($location, $geocode) {
-		if (empty($geocode)) {
-			return false;			
-		} else {
-			$this->load->model('localisation/location');
-			
-			$geocode_info = $this->model_localisation_location->getLocationByGeocode($geocode);
-			
-			if (!$geocode_info) {
-				return false;
-			} else {
-				$this->load->model('setting/extension');
+	public function getZoneToGeoZoneLocation($location) {
+		$this->load->model('setting/extension');
 
-				// Totals
-				$totals = array();
-				$taxes = $this->cart->getTaxes();
-				$total = 0;
+		/ Totals
+		$totals = array();
+		$taxes = $this->cart->getTaxes();
+		$total = 0;
 
-				$sort_order = array();
+		$sort_order = array();
 
-				$results = $this->model_setting_extension->getExtensions('total');
+		$results = $this->model_setting_extension->getExtensions('total');
 
-				foreach ($results as $key => $value) {
-					$sort_order[$key] = $this->config->get('total_' . $value['code'] . '_sort_order');
-				}
+		foreach ($results as $key => $value) {
+			$sort_order[$key] = $this->config->get('total_' . $value['code'] . '_sort_order');
+		}
 
-				array_multisort($sort_order, SORT_ASC, $results);
+		array_multisort($sort_order, SORT_ASC, $results);
 
-				foreach ($results as $result) {
-					if ($this->config->get('total_' . $result['code'] . '_status')) {
-						$this->load->model('extension/total/' . $result['code']);
+		foreach ($results as $result) {
+			if ($this->config->get('total_' . $result['code'] . '_status')) {
+				$this->load->model('extension/total/' . $result['code']);
 
-						// __call can not pass-by-reference so we get PHP to call it as an anonymous function.
-						($this->{'model_extension_total_' . $result['code']}->getTotal)($totals, $taxes, $total);
+				// __call can not pass-by-reference so we get PHP to call it as an anonymous function.
+				($this->{'model_extension_total_' . $result['code']}->getTotal)($totals, $taxes, $total);
 
-					}
-				}
-
-				// Payment Methods
-				$address_data = array();
-
-				$results = $this->model_setting_extension->getExtensions('payment');
-
-				foreach ($results as $result) {
-					if (!empty($this->config->get('payment_' . $result['code'] . '_status') && $this->config->get('payment_' . $result['code'] . '_status') {
-						// Accounts
-						if (!empty($this->config->get('payment_' . $result['code'] . '_location') && $location == $this->config->get('payment_' . $result['code'] . '_location') && $this->config->get('payment_' . $result['code'] . '_location') == 'account') {
-							$address_data['payment_account'][$result['code']] = true;
-						}
-							
-						// Addresses
-						if (!empty($this->config->get('payment_' . $result['code'] . '_location') && $location == $this->config->get('payment_' . $result['code'] . '_location') && $this->config->get('payment_' . $result['code'] . '_location') == 'address') {
-							$address_data['payment_address'][$result['code']] = true;
-						}
-					}
-				}
-
-				$results = $this->model_setting_extension->getExtensions('shipping');
-
-				foreach ($results as $result) {
-					if (!empty($this->config->get('shipping_' . $result['code'] . '_status') && $this->config->get('shipping_' . $result['code'] . '_status') {
-						// Addresses
-						if (!empty($this->config->get('shipping_' . $result['code'] . '_location') && $location == $this->config->get('shipping_' . $result['code'] . '_location') && $this->config->get('shipping_' . $result['code'] . '_location') == 'address') {
-							$address_data['shipping_address'][$result['code']] = true;
-						}
-					}
-				}				
-
-				return $address_data;
 			}
 		}
+
+		// Payment Methods
+		$address_data = array();
+
+		$results = $this->model_setting_extension->getExtensions('payment');
+
+		foreach ($results as $result) {
+			if (!empty($this->config->get('payment_' . $result['code'] . '_status') && $this->config->get('payment_' . $result['code'] . '_status') {
+				// Accounts
+				if (!empty($this->session->data['api_session_id']) && !empty($this->config->get('payment_' . $result['code'] . '_location') && $location == $this->config->get('payment_' . $result['code'] . '_location') && $this->config->get('payment_' . $result['code'] . '_location') == 'account') {
+					$address_data['payment_account'][$result['code']] = true;
+				}
+							
+				// Addresses
+				if (!empty($this->config->get('payment_' . $result['code'] . '_location') && $location == $this->config->get('payment_' . $result['code'] . '_location') && $this->config->get('payment_' . $result['code'] . '_location') == 'address') {
+					$address_data['payment_address'][$result['code']] = true;
+				}
+			}
+		}
+
+		$results = $this->model_setting_extension->getExtensions('shipping');
+
+		foreach ($results as $result) {
+			if (!empty($this->config->get('shipping_' . $result['code'] . '_status') && $this->config->get('shipping_' . $result['code'] . '_status') {
+				// Addresses
+				if (!empty($this->config->get('shipping_' . $result['code'] . '_location') && $location == $this->config->get('shipping_' . $result['code'] . '_location') && $this->config->get('shipping_' . $result['code'] . '_location') == 'address') {
+					$address_data['shipping_address'][$result['code']] = true;
+				}
+			}
+		}
+				    
+		return $address_data;
 	}
 }
