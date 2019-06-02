ebb51d734fa1c8fdad7ff8f898eb3eaaa67fdc82
straightlight <32371528+straightlight@users.noreply.github.com>
2019-06-02 19:17:26 -0400

    Update customer_search.php

--- upload/admin/language/en-gb/extension/report/customer_search.php
+++ upload/admin/language/en-gb/extension/report/customer_search.php
@@ -9,23 +9,44 @@ $_['text_success']      = 'Success: You have modified customer searches report!'
 $_['text_filter']       = 'Filter';
 $_['text_guest']        = 'Guest';
 $_['text_customer']     = '<a href="%s">%s</a>';
+$_['text_year']         = 'Years';
+$_['text_month']        = 'Months';
+$_['text_week']         = 'Weeks';
+$_['text_day']          = 'Days';
 
 // Column
-$_['column_keyword']    = 'Keyword';
-$_['column_products']   = 'Found Products';
-$_['column_category']   = 'Category';
-$_['column_customer']   = 'Customer';
-$_['column_ip']         = 'IP';
-$_['column_date_added'] = 'Date Added';
+$_['column_keyword']    		= 'Keyword';
+$_['column_products']   		= 'Products';
+$_['column_searches']			= 'Searches';
+$_['column_categories']   		= 'Categories';
+$_['column_product']			= 'Product';
+$_['column_customer']   		= 'Customer';
+$_['column_customer_group'] 	= 'Customer Group';
+$_['column_country']			= 'Country';
+$_['column_zone']				= 'Zone';
+$_['column_store']				= 'Store';
+$_['column_payment']			= 'Payment';
+$_['column_shipping']			= 'Shipping';
+$_['column_tax']				= 'Tax';
+$_['column_total']				= 'Total';
+$_['column_recurring']			= 'Recurring';
+$_['column_payment_method'] 	= 'Payment';
+$_['column_shipping_method']	= 'Shipping';
+$_['column_date_added'] 		= 'Date';
 
 // Entry
-$_['entry_date_start']  = 'Date Start';
-$_['entry_date_end']    = 'Date End';
-$_['entry_keyword']     = 'Keyword';
-$_['entry_customer']    = 'Customer';
-$_['entry_ip']          = 'IP';
-$_['entry_status']      = 'Status';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_date_start']  	= 'Date Start';
+$_['entry_date_end']    	= 'Date End';
+$_['entry_keyword']     	= 'Keyword';
+$_['entry_customer']    	= 'Customer';
+$_['entry_customer_group']	= 'Customer Group';
+$_['entry_country']			= 'Country';
+$_['entry_zone']			= 'Zone';
+$_['entry_product']			= 'Product';
+$_['entry_group']			= 'Group By';
+$_['entry_ip']          	= 'IP';
+$_['entry_status']      	= 'Status';
+$_['entry_sort_order']  	= 'Sort Order';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify customer searches report!';
