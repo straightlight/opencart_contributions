0800d2f07fc0d83e5f2eac96a510126769d99a65
straightlight <32371528+straightlight@users.noreply.github.com>
2019-06-13 13:48:19 -0400

    Update bestseller.php

--- upload/catalog/controller/extension/module/bestseller.php
+++ upload/catalog/controller/extension/module/bestseller.php
@@ -99,4 +99,91 @@ class ControllerExtensionModuleBestSeller extends Controller {
 			return $this->load->view('extension/module/bestseller', $data);
 		}
 	}
+	
+	// catalog/model/account/search/addSearch/before
+	public function getBestSellersEvent(&$route, &$args) {
+		$this->load->language('mail/bestsellers');
+		
+		$data['bestsellers'] = array();
+
+		$category_id = 0;
+		
+		$sub_categories = array();
+		
+		if (isset($this->request->get['path'])) {
+			$parts = explode('_', (string)$this->request->get['path']);
+
+			$category_id = (int)array_pop($parts);
+
+			foreach ($parts as $path_id) {
+				$sub_categories[] = $path_id;
+			}
+		}
+		
+		if (isset($this->request->get['product_id'])) {
+			$product_id = $this->request->get['product_id'];
+		} else {
+			$product_id = 0;
+		}
+		
+		$setting_data = array('group'				=> $this->config->get('module_bestsellers_group'),
+							  'type_order'			=> $this->config->get('module_bestsellers_type_order'),
+							  'limit'				=> $this->config->get('module_bestsellers_limit'),
+							 );
+		
+		$filter_data = array('category_id'			=> $category_id,
+							 'sub_categories'		=> $sub_categories,
+							 'product_id'			=> $product_id,
+							 'privilege'			=> true,
+							 'filter'				=> 'product',
+							);
+
+		$this->load->model('catalog/product');
+		
+		$bestsellers = $this->model_catalog_product->getBestSellerProducts($setting_data, $filter_data);
+		
+		if ($bestsellers) {
+			$this->load->model('checkout/order');
+			
+			foreach ($bestsellers as $bestseller) {
+				$product_recurring = $this->db->query("SELECT * FROM `" . DB_PREFIX . "order_recurring` WHERE `product_id` = '" . (int)$product_id . "'");
+				
+				if (!$product_recurring || $product_recurring['status']) {
+					$order_info = $this->model_checkout_order->getOrder($bestseller['order_product_order_id']);
+					
+					if ($order_info) {
+						$data['bestsellers'][] = array('firstname'				=> $order_info['firstname'],
+													   'lastname'				=> $order_info['lastname'],
+													   'email'					=> $order_info['email'],
+													   'date_added'				=> date($this->language->get('date_format'), $order_info['date_added']),
+													   'date_modified'			=> date($this->language->get('date_format'), $order_info['date_modified']),
+													   'order_product_name'		=> $bestseller['order_product_name'],
+													   'products'				=> $bestseller['products'],
+													   'searches'				=> $bestseller['searches'],
+													  );
+						
+						$this->model_checkout_order->setOrderProductPrivileges($bestseller['order_product_order_id']);
+					}
+				}
+			}
+			
+			// Mail
+			$mail = new Mail($this->config->get('config_mail_engine'));
+			
+			$mail->parameter = $this->config->get('config_mail_parameter');
+			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
+			$mail->smtp_username = $this->config->get('config_mail_smtp_username');
+			$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
+			$mail->smtp_port = $this->config->get('config_mail_smtp_port');
+			$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
+
+			$mail->setTo($this->config->get('config_email'));
+			$mail->setFrom($this->config->get('config_email'));
+			$mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
+			$mail->setSubject(html_entity_decode(sprintf($this->language->get('text_subject'), $this->config->get('config_name')), ENT_QUOTES, 'UTF-8'));
+			$mail->setText($this->load->view('mail/bestsellers', $data));
+			
+			$mail->send();
+		}
+	}
 }
