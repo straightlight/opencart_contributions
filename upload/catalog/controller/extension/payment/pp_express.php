7d3243a4346d7fbab108523ce2f56c3dad47b2c9
straightlight <32371528+straightlight@users.noreply.github.com>
2019-06-13 14:08:59 -0400

    Update pp_express.php

--- upload/catalog/controller/extension/payment/pp_express.php
+++ upload/catalog/controller/extension/payment/pp_express.php
@@ -239,6 +239,8 @@ class ControllerExtensionPaymentPpExpress extends Controller {
 				// Address Verification (Business Users)
 				if (!empty($address_verify['ACK']) && strtoupper($address_verify['ACK']) == 'SUCCESS' && !empty($address_verify['CONFIRMATIONCODE']) && strtoupper($address_verify['CONFIRMATIONCODE']) == 'CONFIRMED' && !empty($address_verify['STREETMATCH']) && strtoupper($address_verify['STREETMATCH']) == 'MATCHED' && !empty($address_verify['COUNTRYCODE'])) {
 					$country_info = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE UCASE(TRIM(`iso_code_2`)) = '" . $this->db->escape(strtoupper($address_verify['COUNTRYCODE'])) . "' AND `status` = '1' LIMIT 1")->row;
+					
+					$this->model_extension_payment_pp_express->log($data['METHOD'] . ' :: AddressVerify :: ' . print_r($address_verify));
 				} else {
 					$country_info = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE UCASE(TRIM(`iso_code_2`)) = '" . $this->db->escape(strtoupper($result['PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE'])) . "' AND `status` = '1' LIMIT 1")->row;
 				}
