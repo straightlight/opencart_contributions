7065ee071eb673d597e65385520e30bb7c1e5ada
straightlight <32371528+straightlight@users.noreply.github.com>
2019-06-16 10:07:45 -0400

    Create bestseller.php

---
+++ upload/catalog/language/en-gb/mail/bestseller.php
@@ -0,0 +1,15 @@
+<?php
+// Text
+$_['text_subject']          		= '%s - Bestsellers';
+$_['text_products']         		= 'Products';
+$_['text_total']            		= 'Total';
+$_['text_order_period_day'] 		= '%s day interval';
+$_['text_order_period_days']		= '%s days interval';
+$_['text_order_period_week']		= '%s week interval';
+$_['text_order_period_weeks']		= '%s weeks interval';
+$_['text_order_period_month']		= '%s week interval';
+$_['text_order_period_months']	= '%s months interval';
+$_['text_order_period_year']		= '%s year interval';
+$_['text_order_period_years']		= '%s years interval';
+$_['text_order_period_months']	= '%s months interval';
+$_['text_footer']           		= 'Please reply to this e-mail if you have any questions.';
