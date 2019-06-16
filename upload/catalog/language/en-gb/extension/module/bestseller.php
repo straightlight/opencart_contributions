083e1d30fa0c034f3600b822c7bda2379187ca02
straightlight <32371528+straightlight@users.noreply.github.com>
2019-06-16 10:07:05 -0400

    Update bestseller.php

--- upload/catalog/language/en-gb/extension/module/bestseller.php
+++ upload/catalog/language/en-gb/extension/module/bestseller.php
@@ -1,16 +1,7 @@
 <?php
+// Heading
+$_['heading_title'] = 'Bestsellers';
+
 // Text
-$_['text_subject']          		= '%s - Bestsellers';
-$_['text_customer_search']      = 'Searched %s time(s)';
-$_['text_products']         		= 'Products';
-$_['text_total']            		= 'Total';
-$_['text_order_period_day'] 		= '%s day interval';
-$_['text_order_period_days']		= '%s days interval';
-$_['text_order_period_week']		= '%s week interval';
-$_['text_order_period_weeks']		= '%s weeks interval';
-$_['text_order_period_month']		= '%s week interval';
-$_['text_order_period_months']		= '%s months interval';
-$_['text_order_period_year']		= '%s year interval';
-$_['text_order_period_years']		= '%s years interval';
-$_['text_order_period_months']		= '%s months interval';
-$_['text_footer']           		= 'Please reply to this e-mail if you have any questions.';
+$_['text_tax']      = 'Ex Tax:';
+$_['text_customer_search'] = 'Searched %s time(s)';
