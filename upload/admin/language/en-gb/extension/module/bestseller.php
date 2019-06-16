fd72453037049248fdb72d0495a911aec8496015
straightlight <32371528+straightlight@users.noreply.github.com>
2019-06-16 10:24:02 -0400

    Update bestseller.php

--- upload/admin/language/en-gb/extension/module/bestseller.php
+++ upload/admin/language/en-gb/extension/module/bestseller.php
@@ -1,44 +1,50 @@
 <?php
 // Heading
-$_['heading_title']    = 'Bestsellers';
+$_['heading_title']     = 'Customer Searches Report';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified bestsellers module!';
-$_['text_edit']        = 'Edit Bestsellers Module';
-$_['text_type_asc']    = 'Ascending';
-$_['text_type_desc']   = 'Descending';
-$_['text_type_total']  = 'Total';
-$_['text_type_search'] = 'Search';
-$_['text_database_transaction_delete'] = 'Delete';
-$_['text_database_transaction_update'] = 'Update';
-$_['text_groups']      = array('day'        => 'day',
-                               'week'       => 'week',
-                               'month'      => 'month',
-                               'year'       => 'year',
-                              );
+$_['text_extension']    = 'Extensions';
+$_['text_edit']         = 'Edit Customer Searches Report';
+$_['text_success']      = 'Success: You have modified customer searches report!';
+$_['text_filter']       = 'Filter';
+$_['text_guest']        = 'Guest';
+$_['text_customer']     = '<a href="%s">%s</a>';
+$_['text_salesrep_min'] = 'Min';
+$_['text_salesrep_max'] = 'Max';
+$_['text_year']         = 'Years';
+$_['text_month']        = 'Months';
+$_['text_week']         = 'Weeks';
+$_['text_day']          = 'Days';
+
+// Column
+$_['column_date_start'] 		= 'Date Start';
+$_['column_date_end'] 			= 'Date End';
+$_['column_order']				= 'Order #';
+$_['column_keyword']    		= 'Keyword';
+$_['column_products']   		= 'Products';
+$_['column_searches']			= 'Searches';
+$_['column_categories']   		= 'Categories';
+$_['column_product']			= 'Product';
+$_['column_customer']   		= 'Customer';
+$_['column_customer_group'] 	= 'Customer Group';
+$_['column_tax']				= 'Tax';
+$_['column_total']				= 'Total';
+$_['column_payment_method'] 	= 'Payment';
+$_['column_shipping_method']	= 'Shipping';
 
 // Entry
-$_['entry_name']       = 'Module Name';
-$_['entry_limit']      = 'Limit';
-$_['entry_image']      = 'Image (W x H) and Resize Type';
-$_['entry_width']      = 'Width';
-$_['entry_height']     = 'Height';
-$_['entry_type']	   = 'Type';
-$_['entry_type_order'] = 'Type Order';
-$_['entry_database_transaction'] = 'Database Transaction';
-$_['entry_group']      = 'Group By';
-$_['entry_rating']     = 'Rating';
-$_['entry_order_period_notify'] = 'Event Notify';
-$_['entry_order_period_value'] = 'Period value';
-$_['entry_status']     = 'Status';
+$_['entry_date_start']  	= 'Date Start';
+$_['entry_date_end']    	= 'Date End';
+$_['entry_keyword']     	= 'Keyword';
+$_['entry_customer']    	= 'Customer';
+$_['entry_customer_group']	= 'Customer Group';
+$_['entry_category']		= 'Category';
+$_['entry_product']			= 'Product';
+$_['entry_group']			= 'Group By';
+$_['entry_ip']          	= 'IP';
+$_['entry_salesrep']		= 'Sales Reputation';
+$_['entry_status']      	= 'Status';
+$_['entry_sort_order']  	= 'Sort Order';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify bestsellers module!';
-$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
-$_['error_width']      = 'Width required!';
-$_['error_height']     = 'Height required!';
-$_['error_order_period_value_day'] = 'Error: The selected day must be between 1 and 365 days!';
-$_['error_order_period_value_week'] = 'Error: The selected week must be between 1 and 52 weeks!';
-$_['error_order_period_value_month'] = 'Error: The selected month must be between 1 and 12 months!';
-$_['error_order_period_value_year'] = 'Error: The selected year must be 1 year!';
+$_['error_permission']  = 'Warning: You do not have permission to modify customer searches report!';
