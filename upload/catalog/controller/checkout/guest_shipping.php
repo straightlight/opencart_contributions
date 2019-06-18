865646dff6124abc4a439ce204f368bf30bce28e
straightlight <32371528+straightlight@users.noreply.github.com>
2019-06-18 14:08:00 -0400

    Update guest_shipping.php

--- upload/catalog/controller/checkout/guest_shipping.php
+++ upload/catalog/controller/checkout/guest_shipping.php
@@ -88,7 +88,7 @@ class ControllerCheckoutGuestShipping extends Controller {
 
 		// Validate if customer is logged in.
 		if ($this->customer->isLogged()) {
-			$json['redirect'] = $this->url->link('checkout/checkout', '', true);
+			$json['redirect'] = $this->url->link('checkout/checkout', '&language=' . $this->config->get('config_language'));
 		}
 
 		// Validate cart has products and has stock.
@@ -98,7 +98,7 @@ class ControllerCheckoutGuestShipping extends Controller {
 
 		// Check if guest checkout is available.
 		if (!$this->config->get('config_checkout_guest') || $this->config->get('config_customer_price') || $this->cart->hasDownload()) {
-			$json['redirect'] = $this->url->link('checkout/checkout', '', true);
+			$json['redirect'] = $this->url->link('checkout/checkout', '&language=' . $this->config->get('config_language'));
 		}
 
 		if (!$json) {
