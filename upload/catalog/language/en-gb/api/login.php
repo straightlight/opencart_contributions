0b49ea37bf856b252f3a621f15935b141a80f3c5
straightlight <32371528+straightlight@users.noreply.github.com>
2019-07-04 21:18:45 -0400

    Update login.php

--- upload/catalog/language/en-gb/api/login.php
+++ upload/catalog/language/en-gb/api/login.php
@@ -6,3 +6,4 @@ $_['text_success'] 			= 'Success: API session successfully started!';
 $_['error_permission'] 		= 'Warning: You do not have permission to access the API!';
 $_['error_key']    			= 'Warning: Incorrect API Key!';
 $_['error_ip']     			= 'Warning: Your IP %s is not allowed to access this API!';
+$_['error_login']			= 'Warning: Login authentication could not be found!';
