a01ea0e3ba516c983c68e1557e416cce3926c197
straightlight <32371528+straightlight@users.noreply.github.com>
2019-07-08 20:08:34 -0400

    Update login.php

--- upload/catalog/language/en-gb/api/login.php
+++ upload/catalog/language/en-gb/api/login.php
@@ -7,3 +7,5 @@ $_['error_permission'] 		= 'Warning: You do not have permission to access the AP
 $_['error_key']    			= 'Warning: Incorrect API Key!';
 $_['error_ip']     			= 'Warning: Your IP %s is not allowed to access this API!';
 $_['error_login']			= 'Warning: Login authentication could not be found!';
+$_['error_code'] = 'Warning: Invalid Store Code!';
+$_['error_hash'] = 'Warning: Invalid Store Hash!';
