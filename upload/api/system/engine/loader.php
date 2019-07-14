f02713f3cf230d9ffb0240e08f2ea729fa71f8f5
straightlight <32371528+straightlight@users.noreply.github.com>
2019-07-14 10:12:36 -0400

    Update loader.php

--- upload/api/system/engine/loader.php
+++ upload/api/system/engine/loader.php
@@ -52,4 +52,50 @@ final class ApiLoader {
 			}
 		}
 	}
+	
+	protected function callback($registry, $route) {
+		return function($args) use($registry, $route) {
+			static $model;
+			
+			$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
+
+			// Keep the original trigger
+			$trigger = $route;
+					
+			// Trigger the pre events
+			$result = $registry->get('event')->trigger('api/model/' . $trigger . '/before', array(&$route, &$args));
+			
+			if ($result && !$result instanceof Exception) {
+				$output = $result;
+			} else {
+				$class = 'ApiModel' . preg_replace('/[^a-zA-Z0-9]/', '', substr($route, 0, strrpos($route, '/')));
+				
+				// Store the model object
+				$key = substr($route, 0, strrpos($route, '/'));
+				
+				if (!isset($model[$key])) {
+					$model[$key] = new $class($registry);
+				}
+				
+				$method = substr($route, strrpos($route, '/') + 1);
+				
+				$callable = array($model[$key], $method);
+	
+				if (is_callable($callable)) {
+					$output = call_user_func_array($callable, $args);
+				} else {
+					throw new \Exception('Error: Could not call model/' . $route . '!');
+				}					
+			}
+			
+			// Trigger the post events
+			$result = $registry->get('event')->trigger('api/model/' . $trigger . '/after', array(&$route, &$args, &$output));
+			
+			if ($result && !$result instanceof Exception) {
+				$output = $result;
+			}
+						
+			return $output;
+		};
+	}	
 }
