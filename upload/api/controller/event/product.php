28d3d5ad15337ba0f0b00b7eb16b50a48f0228a1
straightlight <32371528+straightlight@users.noreply.github.com>
2020-10-12 20:26:40 -0400

    Added int to page request.

--- upload/api/controller/event/product.php
+++ upload/api/controller/event/product.php
@@ -58,7 +58,7 @@ class ApiControllerEventApi extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}
